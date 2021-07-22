761f952803c34b1605e3aeb060e867e88fe48feb
최용화 <yonghwa9086@naver.com>
2021-07-22 17:04:26 +0900

    Time: 1 ms (99.26%), Space: 37.4 MB (95.90%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -6,7 +6,8 @@ class Solution {
         }
 
         for (String word : wordDict) {
-            findWord(s, word, endIndexOfWord);
+            int[] pi = getPiArray(word);
+            findWord(s, word, pi, endIndexOfWord);
         }
         boolean[] isSegmented = new boolean[s.length()];
         for (int i = 0; i < s.length(); i++) {
@@ -15,18 +16,42 @@ class Solution {
         return canBeSegmented(s, 0, endIndexOfWord, isSegmented);
     }
     
-    private static void findWord(String s, String word, List<Integer>[] endIndexOfWord) {
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < word.length() && (i + j) < s.length(); j++) {
-                if (s.charAt(i + j) != word.charAt(j)) {
-                    break;
+    private static void findWord(String str, String pattern, int[] pi, List<Integer>[] endIndexOfWord) {
+        for (int i = 0, j = 0; i + pattern.length() <= str.length(); ) {
+            if (str.charAt(i + j) == pattern.charAt(j)) {
+                j++;
+                if (j >= pattern.length()) {
+                    endIndexOfWord[i].add(i + j - 1);
+                    i += j - pi[j - 1];
+                    j = pi[j - 1];
                 }
-
-                if (j == word.length() - 1) {
-                    endIndexOfWord[i].add(i + j);
+            } else {
+                if (j > 0) {
+                    i += j - pi[j - 1];
+                    j = pi[j - 1];
+                } else {
+                    i++;
                 }
             }
         }
+    }
+    
+    private static int[] getPiArray(String str) {
+        int[] pi = new int[str.length()];
+        for (int i = 1, j = 0; i + j < str.length(); ) {
+            if (str.charAt(i + j) == str.charAt(j)) {
+                pi[i + j] = j + 1;
+                j++;
+            } else {
+                if (j > 0) {
+                    i += j - pi[j - 1];
+                    j = pi[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return pi;
     }
 
     private static boolean canBeSegmented(String s, int index, List<Integer>[] endIndexOfWord, boolean[] isSegmented) {
