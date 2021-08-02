344d918662cecc3e1d34eb4a7906859c58593fea
최용화 <yonghwa9086@naver.com>
2021-08-03 00:59:55 +0900

    Time: 0 ms (100.00%), Space: 39.4 MB (44.37%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        getSubset(0, nums, new LinkedList<>(), answer);
+        getSubset(0, nums, new ArrayList<>(), answer);
         return answer;
     }
     
     private static void getSubset(int index, int[] nums, List<Integer> subset, List<List<Integer>> answer) {
         if (index >= nums.length) {
-            answer.add(new LinkedList<>(subset));
+            answer.add(new ArrayList<>(subset));
             return;
         }
         subset.add(nums[index]);
