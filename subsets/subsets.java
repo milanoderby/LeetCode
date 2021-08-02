7a4df57eecb7023e7ebc43c3f18f55740d44f6a5
최용화 <yonghwa9086@naver.com>
2021-08-03 01:22:22 +0900

    Time: 0 ms (100.00%), Space: 39.3 MB (44.37%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        getSubset(0, nums, new ArrayList<>(), answer);
+        makeSubset(0, nums, new ArrayList<>(), answer);
         return answer;
     }
     
-    private static void getSubset(int index, int[] nums, List<Integer> subset, List<List<Integer>> answer) {
+    private static void makeSubset(int index, int[] nums, List<Integer> subset, List<List<Integer>> answer) {
         if (index >= nums.length) {
             answer.add(new ArrayList<>(subset));
             return;
         }
         subset.add(nums[index]);
-        getSubset(index + 1, nums, subset, answer);
+        makeSubset(index + 1, nums, subset, answer);
         subset.remove(subset.size() - 1);
-        getSubset(index + 1, nums, subset, answer);
+        makeSubset(index + 1, nums, subset, answer);
     }
 }
