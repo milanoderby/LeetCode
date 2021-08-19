5d8347c8372b6d3fab19e73e81ed9912ef379bfa
최용화 <yonghwa9086@naver.com>
2021-08-20 07:30:45 +0900

    Time: 0 ms (100.00%), Space: 39.1 MB (47.91%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -16,23 +16,23 @@
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> arrayList = new ArrayList<>();
-        getCountOfSubTree(root, arrayList, 0);
+        getCountOfSubTree(root, arrayList, 0, k);
 
         return arrayList.get(k - 1);
     }
     
-    private static int getCountOfSubTree(TreeNode root, List<Integer> arrayList, int countOfSubTree) {
-        if (Objects.isNull(root)) {
+    private static int getCountOfSubTree(TreeNode root, List<Integer> arrayList, int countOfSubTree, int k) {
+        if (Objects.isNull(root) || arrayList.size() > k) {
             return countOfSubTree;
         }
 
         if (Objects.nonNull(root.left)) {
-            countOfSubTree += getCountOfSubTree(root.left, arrayList, countOfSubTree);
+            countOfSubTree += getCountOfSubTree(root.left, arrayList, countOfSubTree, k);
         }
         arrayList.add(root.val);
 
         if (Objects.nonNull(root.right)) {
-            countOfSubTree += getCountOfSubTree(root.right, arrayList, countOfSubTree);
+            countOfSubTree += getCountOfSubTree(root.right, arrayList, countOfSubTree, k);
         }
 
         return countOfSubTree;
