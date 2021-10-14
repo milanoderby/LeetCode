f56afa888ef2108ea619061660c4a8fba8052279
milanoderby <yonghwa9086@naver.com>
2021-10-14 23:57:09 +0900

    Time: 0 ms (100.00%), Space: 37.8 MB (22.56%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,14 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] countOfPath = new int[m + 1][n + 1];
-        countOfPath[0][0] = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                countOfPath[i + 1][j] += countOfPath[i][j];
-                countOfPath[i][j + 1] += countOfPath[i][j];
+        int[][] grid = new int[m + 1][n + 1];
+        grid[0][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
             }
         }
-
-        return countOfPath[m - 1][n - 1];
+        return grid[m][n];
     }
 }
