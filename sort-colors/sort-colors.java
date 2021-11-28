3ae53ef3d78155f921c650d111ac68b6e7d058b5
최용화 <yonghwa.choi@navercorp.com>
2021-11-28 14:51:08 +0900

    Time: 0 ms (100.00%), Space: 37.6 MB (59.49%) - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int index = 0;
+        int indexOfZero = 0;
+        int indexOfTwo = nums.length - 1;
+
+        while (index <= indexOfTwo) {
+            if (nums[index] == 0) {
+                swap(indexOfZero, index, nums);
+                indexOfZero++;
+                index++;
+            } else if (nums[index] == 1) {
+                index++;
+            } else {
+                swap(index, indexOfTwo, nums);
+                indexOfTwo--;
+            }
+        }
+    }
+
+    private static void swap(int index1, int index2, int[] nums) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
