3e50459f4eb4d84aa52c83319fc29ee3451bbf78
최용화 <yonghwa9086@naver.com>
2022-04-24 20:03:11 +0900

    Time: 1 ms (87.99%), Space: 54.5 MB (20.85%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,30 +1,49 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean[] isRowZero = new boolean[matrix.length];
-        boolean[] isColumnZero = new boolean[matrix[0].length];
-
+        boolean setFirstRowToZero = false;
+        boolean setFirstColToZero = false;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    isRowZero[i] = true;
-                    isColumnZero[j] = true;
+                    if (i == 0) {
+                        setFirstRowToZero = true;
+                    }
+
+                    if (j == 0) {
+                        setFirstColToZero = true;
+                    }
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            if (isRowZero[i]) {
+        for (int i = 1; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
                 for (int j = 0; j < matrix[0].length; j++) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        for (int j = 0; j < matrix[0].length; j++) {
-            if (isColumnZero[j]) {
+        for (int j = 1; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
                 for (int i = 0; i < matrix.length; i++) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if (setFirstRowToZero) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (setFirstColToZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
