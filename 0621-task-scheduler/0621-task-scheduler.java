9a8ef154a74e3577ec462a9997d8e5af087ff860
최용화 <yonghwa9086@naver.com>
2024-03-19 22:07:31 +0900

    Time: 42 ms (11.77%), Space: 45.4 MB (75.85%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // 현재 cycle 에서 실행가능한 Task 중 갯수가 가장 많이 남은 Task를 실행합니다.
+        // 현재, 실행가능한 Task을 판단하는 여부: (최근 실행시점과 현재시점과의 차이) >= n일 경우, 실행 가능
+
+        int[] numberOfTask = new int[26];
+        for (int i = 0; i < tasks.length; i++) {
+            int task = tasks[i] - 'A';
+            numberOfTask[task]++;
+        }
+
+        // 모든 Task는 0초에 처리되었다고 가정
+        int[] timeOfTaskCompleted = new int[26];
+        for (int i = 0; i < 26; i++) {
+            timeOfTaskCompleted[i] = -1 * n;
+        }
+        
+        // 현재 시점은 n초라고 가정
+        // 즉, n초에는 모든 Task를 실행 가능
+
+        int sumOfTask = tasks.length;
+        int currentTime;
+        for (currentTime = 1; sumOfTask > 0; currentTime++) {
+            int completableTask = -1;
+            int maxNumberOfTask = -1;
+            for (int i = 0; i < 26; i++) {
+                if (numberOfTask[i] <= 0) {
+                    continue;
+                }
+
+                if (currentTime - timeOfTaskCompleted[i] <= n) {
+                    continue;
+                }
+
+                if (maxNumberOfTask < numberOfTask[i]) {
+                    maxNumberOfTask = numberOfTask[i];
+                    completableTask = i;
+                }
+            }
+
+            if (completableTask != -1) {
+                numberOfTask[completableTask]--;
+                sumOfTask--;
+
+                timeOfTaskCompleted[completableTask] = currentTime;
+            }
+        }
+        return currentTime - 1;
+    }
+}
