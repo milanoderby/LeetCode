0ab28621d45688f04231abc5d69c4c0ca35a5874
최용화 <yonghwa9086@naver.com>
2024-03-24 17:34:28 +0900

    Time: 2 ms (97.31%), Space: 59.7 MB (6.65%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int[] count = new int[nums.length];
+        for (int num : nums) {
+            count[num]++;
+            if (count[num] >= 2) {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
